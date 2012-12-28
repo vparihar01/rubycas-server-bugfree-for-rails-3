1fffa74c950d257f879b77928fb3fda61413029f
Vivek Parihar <vivek@weboniselab.com>
2012-12-28 18:07:46 +0530

    lib/casserver errors removed+made compatible with rails 3

--- Gemfile
+++ Gemfile
@@ -1,7 +1,12 @@
 source "http://rubygems.org"
 gemspec
 
-
+gem "rails", '3.2.8'
+gem "mysql2"
+gem "picnic"
+gem "hoe"
+gem "gettext"
+gem "bcrypt-ruby"
 # Gems for authenticators
 group :ldap do
     gem "net-ldap", "~> 0.1.1"
--- lib/casserver/authenticators/sql_encrypted.rb
+++ lib/casserver/authenticators/sql_encrypted.rb
@@ -52,7 +52,7 @@ class CASServer::Authenticators::SQLEncrypted < CASServer::Authenticators::SQL
     username_column = @options[:username_column] || "username"
     encrypt_function = @options[:encrypt_function] || 'user.encrypted_password == Digest::SHA256.hexdigest("#{user.encryption_salt}::#{@password}")'
 
-    $LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.instance_variable_get(:@checked_out).length}/#{user_model.connection_pool.instance_variable_get(:@connections).length}"
+    #$LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.instance_variable_get(:@checked_out).length}/#{user_model.connection_pool.instance_variable_get(:@connections).length}"
     results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
     user_model.connection_pool.checkin(user_model.connection)
     
